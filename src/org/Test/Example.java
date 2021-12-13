13ec2908d3f4db448e17722ec2331ec1f5dcf59d
Dhivya1725 <lakshmidhivya21jyly@gmail.com>
2021-12-13 22:38:04 +0530

    master

---
+++ src/org/Test/Example.java
@@ -0,0 +1,18 @@
+package org.Test;
+
+public class Example {
+	public static void main(String[] args) {
+		int count=1;
+		while(count<=15)
+		{
+			System.out.println((count%2==1)? "@@@@@":"!!!!!!");
+			++count;
+			}
+		int x=3,y=5,z=10,c;
+		
+		c=++z+y-y+z+x++;
+		System.out.println(c);
+		
+	}
+
+}
